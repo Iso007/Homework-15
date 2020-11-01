7a02e7d1941d68280d1fa6503ac7f5f6bc8c0f4d
Iso007 <isodiallo007@gmail.com>
2020-11-01 17:04:29 -0500

    api and html routes set and models need tweeking, about to push server.js

---
+++ 02-Homework/Develop/models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Data,
+        default: Data.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+            name: {
+                type: String,
+                trim: true
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sers: {
+                type: Number
+            }
+        }
+    ]
+});
+
+WorkoutSchema.virtual("totalDuration").get(function(){
+    return this.exercises.reduce(total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const workout = mongoose.model("Workout", WorkoutSchema);
+
+modele.exports = workout;
---
+++ 02-Homework/Develop/routes/api_routes.js
@@ -0,0 +1,48 @@
+const { model } = require("mongoose");
+var db = require("../models");
+
+model.exports = function(app){
+    app.get("/api/workouts", function(req, res){
+        db.workout.find({})
+        .then(doWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.put("/api/workout/:id", function({body, params}, res){
+        db.workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            {new: true}
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts", (req, res)=> {
+        db.workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", function(req, res){
+        db.workout.find({}).limit(5)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+}
